232a35d1594725c9e7751ca6b71ebd4d949c78b9
Rodion <Radium-ON@outlook.com>
2019-11-05 20:42:48 +0700

    Проекты модулей добавлены в решение.

---
+++ CodeMatrixModule/CodeMatrixModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using CodeMatrixModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace CodeMatrixModule
+{
+    public class CodeMatrixModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ HammingCorrectorModule/HammingCorrectorModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using HammingCorrectorModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace HammingCorrectorModule
+{
+    public class HammingCorrectorModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
