34735f142b761dbd4831eb4cf6ee5ec6608b09af
Rodion <Radium-ON@outlook.com>
2019-11-07 17:00:00 +0700

    MourCodeView in region

---
+++ GilbertMourEncoding/GilbertMourCodeAlgorithm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GilbertMourEncoding.Models;
+
+namespace GilbertMourEncoding
+{
+    public class GilbertMourCodeAlgorithm
+    {
+        public struct CodeEntry
+        {
+            public CodeEntry(char xi, double pxi, double q, double sigma, int li, string sigmaBinary, string sigmaLimit)
+            {
+                Xi = xi;
+                Pxi = pxi;
+                Q = q;
+                Sigma = sigma;
+                Li = li;
+                SigmaBinary = sigmaBinary;
+                SigmaLimit = sigmaLimit;
+            }
+
+            public char Xi { get; set; }
+            public double Pxi { get; set; }
+            public double Q { get; set; }
+            public double Sigma { get; set; }
+            public int Li { get; set; }
+            public string SigmaBinary { get; set; }
+            public string SigmaLimit { get; set; }
+        }
+
+        public GilbertMourCodeAlgorithm()
+        {
+            
+        }
+        public GilbertMourCodeAlgorithm(ICollection<TableRecord.CharStats> stats)
+        {
+            foreach (var stat in stats)
+            {
+                _chars.Add(stat.Sign);
+                _probabilities.Add(stat.Probability);
+            }
+
+            _cumulate = GetCumulateArray(_probabilities).ToList();
+            _sigmas = GetSigmasArray(_probabilities, _cumulate).ToList();
+            _limits = GetSigmaLengthArray(_probabilities).ToList();
+            for (var i = 0; i < stats.Count; i++)
+            {
+                _codes.Add(ConvertFractToBinaryCode(_sigmas[i], _limits[i]));
+                CodeEntries.Add(
+                    new CodeEntry(_chars[i], _probabilities[i],
+                        _cumulate[i],
+                        _sigmas[i],
+                        _limits[i], Convert.ToString($"0.{ConvertFractToBinaryCode(_sigmas[i],10)}"),
+                        _codes[i]));
+            }
+        }
+
+        private List<char> _chars = new List<char>();
+        private List<double> _probabilities = new List<double>();
+        private List<double> _cumulate = new List<double>();
+        private List<double> _sigmas = new List<double>();
+        private List<int> _limits = new List<int>();
+        private List<string> _codes = new List<string>();
+
+        public List<CodeEntry> CodeEntries { get; private set; }=new List<CodeEntry>();
+
+        public string ConvertFractToBinaryCode(double sigma, int limit)
+        {
+            var sb = new StringBuilder();
+            var right = (double)sigma - (int)sigma;
+            for (var i = 0; i < limit; i++)
+            {
+                right = right * 2 - (int)right * 2;
+                sb.Append((int)right);
+                if (right == 1.0)
+                {
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public double[] GetCumulateArray(List<double> probs)
+        {
+            var qList = new double[probs.Count];
+            for (var i = 1; i < probs.Count; i++)
+            {
+                qList[i] = qList[i - 1] + probs[i - 1];
+            }
+
+            return qList;
+
+        }
+
+        public double[] GetSigmasArray(List<double> probs, List<double> qList)
+        {
+            var sigmas = new double[probs.Count];
+            for (var i = 0; i < probs.Count; i++)
+            {
+                sigmas[i] = qList[i] + probs[i] / 2.0;
+            }
+
+            return sigmas;
+        }
+
+        public int[] GetSigmaLengthArray(List<double> probs)
+        {
+            var limits = new int[probs.Count];
+            for (var i = 0; i < probs.Count; i++)
+            {
+                limits[i] = (int)Math.Ceiling(-Math.Log(probs[i] / 2.0, 2));
+            }
+
+            return limits;
+        }
+    }
+}
--- GilbertMourEncoding/ViewModels/GilbertViewModel.cs
+++ GilbertMourEncoding/ViewModels/GilbertViewModel.cs
@@ -2,24 +2,87 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using GilbertMourEncoding.Models;
 
 namespace GilbertMourEncoding.ViewModels
 {
     public class GilbertViewModel : BindableBase
     {
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
+        #region AutoProperties
+        public TableRecord Entropy { get; set; }
+
+
+
+        #endregion
 
         public GilbertViewModel()
         {
-            Message = "Gilbert View from your Prism Module";
+            MourCodingCommand = new DelegateCommand(Coding, CanCoding).ObservesProperty(() => EnterText);
+            MourCollection = new ObservableCollection<GilbertMourCodeAlgorithm.CodeEntry>();
         }
+
+        private bool CanCoding()
+        {
+            return !string.IsNullOrEmpty(EnterText);
+        }
+
+        private void Coding()
+        {
+            var mour = new GilbertMourCodeAlgorithm(CharStatsCollection);
+            MourCollection = new ObservableCollection<GilbertMourCodeAlgorithm.CodeEntry>(mour.CodeEntries);
+        }
+
+
+
+        private void OnTextEnterChanged()
+        {
+            Entropy = new TableRecord(EnterText);
+            CharStatsCollection = new ObservableCollection<TableRecord.CharStats>(Entropy.ListStatsOneChar);
+        }
+
+        #region Backing Fields
+        private string _enterText;
+        private ObservableCollection<TableRecord.CharStats> _charStatsCollection;
+        private bool _isTextEntered;
+        private ObservableCollection<GilbertMourCodeAlgorithm.CodeEntry> _mourCollection;
+
+        #endregion
+
+        #region Properties
+        public string EnterText
+        {
+            get => _enterText;
+            set => SetProperty(ref _enterText, value, OnTextEnterChanged);
+        }
+
+        public ObservableCollection<TableRecord.CharStats> CharStatsCollection
+        {
+            get => _charStatsCollection;
+            set => SetProperty(ref _charStatsCollection, value);
+        }
+
+        public ObservableCollection<GilbertMourCodeAlgorithm.CodeEntry> MourCollection
+        {
+            get => _mourCollection;
+            set => SetProperty(ref _mourCollection, value);
+        }
+
+        public bool IsTextEntered
+        {
+            get => _isTextEntered;
+            set => SetProperty(ref _isTextEntered, value);
+        }
+        #endregion
+
+        #region DelegateCommands
+        public ICommand MourCodingCommand { get; private set; }
+
+
+        #endregion
     }
 }
