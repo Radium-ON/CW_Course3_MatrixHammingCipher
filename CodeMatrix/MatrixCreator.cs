c5373fa9284a4271de918f4e63492fcf317cd47a
Rodion <Radium-ON@outlook.com>
2019-11-12 13:15:52 +0700

    Построение кодовых конструкций для всех блоков сообщения работает (тест пройден).

--- CodeMatrix/MatrixCreator.cs
+++ CodeMatrix/MatrixCreator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodeMatrix
@@ -29,6 +30,64 @@ namespace CodeMatrix
             HammingCodesMatrix = GetHammingCodeMatrix(GeneratingMatrix, 3);
         }
 
+        public static T[,] TransposeMatrix<T>(T[,] matrix)
+        {
+            var rows = matrix.GetLength(0);
+            var columns = matrix.GetLength(1);
+
+            T[,] Tmatrix = new T[columns, rows];
+
+            for (var i = 0; i < rows; i++)
+            {
+                for (var j = 0; j < columns; j++)
+                {
+                    Tmatrix[j, i] = matrix[i, j];
+                }
+            }
+
+            return Tmatrix;
+        }
+
+        public byte[,] GetCodeConstructionsFromCipher(string cipher)
+        {
+            var blocks = SplitIntoBlocks(cipher, 4);
+            var blockLength = blocks[0].Length;
+            var codematrix = new byte[blocks.Length,8];
+            //get block, parse it to byte array and * matrix
+            for (var block = 0; block < blocks.Length; block++)
+            {
+                var bitblock = new byte[blockLength];
+                for (var bit = 0; bit < blockLength; bit++)
+                {
+                    var b = blocks[block];
+                    bitblock[bit] = (byte) char.GetNumericValue(b[bit]);
+                }
+
+                var vector = MultiplyVectorByGMatrix(bitblock, GeneratingMatrix);
+
+                for (var i = 0; i < vector.Length; i++)
+                {
+                    codematrix[block, i] = vector[i];
+                }
+            }
+            
+            return codematrix;
+        }
+
+        private byte[] MultiplyVectorByGMatrix(byte[] bitblock, byte[,] gmatrix)
+        {
+            var gColumns = gmatrix.GetLength(1);
+            var codeconstr = new byte[gColumns + 1];
+            for (var column = 0; column < gColumns; column++)//columns G (7)
+            {
+                var xor = bitblock.Select((t, bit) => t * gmatrix[bit, column]).Sum();
+                codeconstr[column + 1] = (byte)(xor % 2);
+            }
+            //xor check bits
+            codeconstr[0] = (byte)(codeconstr[5] ^ codeconstr[6] ^ codeconstr[7]);
+            return codeconstr;
+        }
+
         private byte[,] GetHammingCodeMatrix(byte[,] gmatrix, int checkbits)
         {
             var rows = gmatrix.GetLength(0);
@@ -53,7 +112,7 @@ namespace CodeMatrix
                     {
                         hammingMatrix[i, j] = transposeMatrix[i, j];
                     }
-                    else if(i==j-4)
+                    else if (i == j - 4)
                     {
                         hammingMatrix[i, j] = 1;
                     }
@@ -86,24 +145,6 @@ namespace CodeMatrix
                 .Select(group => group.Select(item => item.c))
                 .Select(chars => new string(chars.ToArray()));
             return stringSplit.ToArray();
-        }
-
-        public static T[,] TransposeMatrix<T>(T[,] matrix)
-        {
-            var rows = matrix.GetLength(0);
-            var columns = matrix.GetLength(1);
-
-            T[,] Tmatrix = new T[columns, rows];
-
-            for (var i = 0; i < rows; i++)
-            {
-                for (var j = 0; j < columns; j++)
-                {
-                    Tmatrix[j, i] = matrix[i, j];
-                }
-            }
-
-            return Tmatrix;
         }
     }
 }
