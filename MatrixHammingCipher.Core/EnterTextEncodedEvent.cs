44761a25030f4a4260660b468347e579e14e2d1c
Rodion <Radium-ON@outlook.com>
2019-11-12 23:07:21 +0700

    Create Core project to contain events

---
+++ MatrixHammingCipher.Core/EnterTextEncodedEvent.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Prism.Events;
+
+namespace MatrixHammingCipher.Core
+{
+    public class EnterTextEncodedEvent : PubSubEvent<byte[,]>
+    {
+        public EnterTextEncodedEvent()
+        {
+
+        }
+    }
+}
