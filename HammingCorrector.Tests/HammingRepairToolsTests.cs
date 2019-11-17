f7efa3c5328b28748121f3b011810b729c62ee1f
Rodion <Radium-ON@outlook.com>
2019-11-17 22:55:33 +0700

    Заглушки методов и тестовый проект.

---
+++ HammingCorrector.Tests/HammingRepairToolsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HammingCorrector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammingCorrector.Tests
+{
+    [TestClass]
+    public class HammingRepairToolsTests
+    {
+        [TestMethod]
+        public void GetSyndromeListTest()
+        {
+
+        }
+    }
+}
---
+++ HammingCorrector/HammingRepairTools.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HammingCorrector.ViewModels;
+
+namespace HammingCorrector
+{
+    public class HammingRepairTools
+    {
+        public byte[,] HammingCodesMatrix { get; private set; }//исходная проверочная матрица
+        public byte[,] ExtendedMatrix { get; private set; }//расширенная матрица Хэмминга
+
+        private byte[,] GetExtendedMatrix(byte[,] matrix)
+        {
+            return new byte[,] { };
+        }
+
+        private byte[] SyndromeOf(byte[] u)
+        {
+            return new byte[] { };
+        }
+
+        public List<byte[]> GetSyndromeList(IList<byte[]> constructions)
+        {
+            return null;
+        }
+
+        private bool CheckSyndromeForError(byte[] syndrome)
+        {
+            return false;
+        }
+
+        private void RepairBitsInConstruction(byte[] syndrome)
+        {
+
+        }
+    }
+}
